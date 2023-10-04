f2beeaa729a0c6294559fa92c470319096aebf5e
leejinwoo <leejinwoo1126@gmail.com>
2023-10-04 16:14:54 +0900

    섹션4-7: SpringContainer로 ServletContainer통합

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -6,26 +6,25 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 public class HellobootApplication {
 	public static void main(String[] args) {
 		// Spring Container
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
+
+				// Servlet Container
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext -> {
+					servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+							.addMapping("/*");
+				});
+				webServer.start(); // 톰캣 서블릿 컨테이너 동작
+			}
+		};
 		applicationContext.registerBean(HelloController.class); // Bean 등록
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.refresh(); // Bean 초기화
-
-		// Servlet Container
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
-					.addMapping("/*");
-		});
-		webServer.start(); // 톰캣 서블릿 컨테이너 동작
 	}
 }
